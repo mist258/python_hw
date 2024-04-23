915689a5d934a1f3da0bcd2cbb9262a077512594
Nataliia <natalia.kolchuk@gmail.com>
2024-04-23 20:21:58 +0300

    lesson3

--- Lesson_3.py
+++ Lesson_3.py
@@ -8,39 +8,39 @@
 #   >, < меньше більше
 #   при виклику метода len() підраховувати сумму сторін
 
-# class Rectangle:
-#     __slots__ = 'side_x', 'side_y', 'area'
-#
-#     def __init__(self, side_x, side_y):
-#         self.side_x = side_x
-#         self.side_y = side_y
-#         self.area = side_x * side_y
-#
-#     def __mul__(self, other):
-#         return self.area * other.area
-#
-#     def __add__(self, other):
-#         return self.area * other.area
-#
-#     def __eq__(self, other):
-#         return self.area == other.area
-#
-#     def __ne__(self, other):
-#         return self.area != other.area
-#
-#     def __lt__(self, other):
-#         return self.area < other.area
-#
-#     def __gt__(self, other):
-#         return self.area > other.area
-#
-#     def __len__(self):
-#         return self.side_x * 2 + self.side_y * 2
-#
-#
-# rectangle1 = Rectangle(8, 5)
-# rectangle2 = Rectangle(5, 6)
-# print(len(rectangle1))
+class Rectangle:
+    __slots__ = 'side_x', 'side_y', 'area'
+
+    def __init__(self, side_x, side_y):
+        self.side_x = side_x
+        self.side_y = side_y
+        self.area = side_x * side_y
+
+    def __mul__(self, other):
+        return self.area * other.area
+
+    def __add__(self, other):
+        return self.area * other.area
+
+    def __eq__(self, other):
+        return self.area == other.area
+
+    def __ne__(self, other):
+        return self.area != other.area
+
+    def __lt__(self, other):
+        return self.area < other.area
+
+    def __gt__(self, other):
+        return self.area > other.area
+
+    def __len__(self):
+        return self.side_x * 2 + self.side_y * 2
+
+
+rectangle1 = Rectangle(8, 5)
+rectangle2 = Rectangle(5, 6)
+print(len(rectangle1))
 
 
 # створити класс Human (name, age)
@@ -79,11 +79,13 @@ class Prince(Human):
     def find_cinderella(self, cinderellas: list[Cinderella]):
         for cinderella in cinderellas:
             if cinderella.size == self.shoes_size:
+                print(cinderella)
                 return cinderella
             else:
                 print('Not suitable shoes')
 
 
-cinderella1: [Cinderella] = [Cinderella('lisa', 20, 38), Cinderella('nika', 20, 38)]
+cinderella1: [Cinderella] = [Cinderella('lisa', 20, 38),
+                             Cinderella('nika', 21, 35)]
 prince = Prince('john', 20, 38)
 prince.find_cinderella(cinderella1)
